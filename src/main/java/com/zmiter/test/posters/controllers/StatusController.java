23cd6c3bf82a645eb9bd1c32ad160b7a99e7a1de
zmiter <zmiter.mde@gmail.com>
2018-03-10 03:34:55 +0300

    Added CrossOrigin to StatusController

--- src/main/java/com/zmiter/test/posters/controllers/StatusController.java
+++ src/main/java/com/zmiter/test/posters/controllers/StatusController.java
@@ -1,6 +1,7 @@
 package com.zmiter.test.posters.controllers;
 
 import com.zmiter.test.posters.responses.Status;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController {
 
     @GetMapping(value = "/status")
+    @CrossOrigin
     public Status getStatus() {
         return new Status("active");
     }
