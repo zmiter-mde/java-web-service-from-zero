2b52eede9812433e3f90a67f806ab514d8eba77c
zmiter <zmiter.mde@gmail.com>
2018-03-10 09:21:05 +0300

    Removed unnecessary endpoint

--- src/main/java/com/zmiter/test/posters/controllers/PosterUploadController.java
+++ src/main/java/com/zmiter/test/posters/controllers/PosterUploadController.java
@@ -1,7 +1,6 @@
 package com.zmiter.test.posters.controllers;
 
 import com.zmiter.test.posters.responses.FileUploadResult;
-import com.zmiter.test.posters.responses.Status;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,11 +16,6 @@ public class PosterUploadController {
         System.out.println(file.getOriginalFilename());
 
         return new FileUploadResult(true, "File successfully uploaded");
-    }
-
-    @GetMapping(value = "/status2")
-    public Status getStatus() {
-        return new Status("active");
     }
 
 }
